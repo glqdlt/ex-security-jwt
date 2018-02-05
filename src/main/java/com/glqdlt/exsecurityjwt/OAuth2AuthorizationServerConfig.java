225962058415689a6468bf4d387a20e7f3a39061
glqdlt <dlfdnd0725@gmail.com>
2018-02-05 16:20:24 +0900

    jwt completed

--- src/main/java/com/glqdlt/exsecurityjwt/OAuth2AuthorizationServerConfig.java
+++ src/main/java/com/glqdlt/exsecurityjwt/OAuth2AuthorizationServerConfig.java
@@ -1,11 +1,10 @@
 package com.glqdlt.exsecurityjwt;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
@@ -15,31 +14,51 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
-public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter{
+public class OAuth2AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
-//    @Value("${symmetric.key}")
+
+    //    @Value("${symmetric.key}")
     private String privateKey = "123";
 
     @Autowired
     AuthenticationManager authenticationManager;
 
-    public OAuth2AuthorizationServerConfig(){
+    public OAuth2AuthorizationServerConfig() {
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints.tokenStore(tokenStore()).accessTokenConverter(jwtAccessTokenConverter()).authenticationManager(authenticationManager);
     }
+
     @Bean
-    public JwtAccessTokenConverter jwtAccessTokenConverter(){
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
         JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
         tokenConverter.setSigningKey(this.privateKey);
         return tokenConverter;
     }
 
     @Bean
-    public TokenStore tokenStore(){
+    public TokenStore tokenStore() {
         return new JwtTokenStore(jwtAccessTokenConverter());
+    }
+
+
+//    https://stackoverflow.com/questions/38165131/spring-security-oauth2-accept-json
+//    https://docs.spring.io/spring-boot/docs/current/reference/html/boot-features-security.html
+    // 아래 방식은 Authorization 헤더에 foo : bar / id: password 를 넣어서 바로 보내는 식이다.
+//     curl foo:bar@localhost:8080/oauth/token -d grant_type=password -d username=admin -d password=admin1234
+//    -d 는 --date , -u 는 --user를 뜻한다.
+    // curl -u foo:bar localhost:8080/oauth/token -d grant_type=password -d username=admin -d password=admin1234
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+//         아래는 inMemory에 인증 Client를 직접 등록해놓은 것.
+        clients.inMemory()
+                .withClient("foo")
+                .secret("bar")
+                .authorizedGrantTypes("password")
+                .scopes("read", "write", "both");
     }
 
 
--- src/test/java/com/glqdlt/exsecurityjwt/ExSecurityJwtApplicationTests.java
+++ src/test/java/com/glqdlt/exsecurityjwt/ExSecurityJwtApplicationTests.java
@@ -1,16 +1,70 @@
 package com.glqdlt.exsecurityjwt;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.context.WebApplicationContext;
 
-@RunWith(SpringRunner.class)
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@WebAppConfiguration
+@ContextConfiguration
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class ExSecurityJwtApplicationTests {
 
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+
+	private MockMvc mvc;
+
+	@Before
+	public void construnt(){
+		mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+				.build();
+	}
+
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void getToken() throws Exception {
+
+//		http://javadox.com/org.springframework.security.oauth/spring-security-oauth2/1.0.5.RELEASE/org/springframework/security/oauth2/provider/endpoint/TokenEndpoint.java.html
+//		 실제 스프링 시큐리티 개발 스팩은 저렇게 되어있음.
+
+
+//		You need to pass data as form data instead of url parameters. Your command should be ..
+//		curl -u aClient:aSecret --data "grant_type=password&username=mauricio.coder&password=123" -X POST -H "Content-Type:application/x-www-form-urlencoded" http://localhost:9000/oauth/token
+//		라고 하는 코멘트를 보아서 수정해보기로 함.
+
+		MultiValueMap<String,String> map = new LinkedMultiValueMap<>();
+		map.add("username","admin");
+		map.add("password","admin1234");
+		map.add("grant_type","password");
+
+
+		MvcResult mvcResult = mvc.perform(post("http://foo:bar@127.0.0.1:8080/oauth/token").contentType(MediaType.APPLICATION_FORM_URLENCODED).params(map))
+				.andReturn();
+
+		System.out.println("status:"+mvcResult.getResponse().getStatus());
+		System.out.println("body:"+mvcResult.getResponse().getContentAsString());
+
+
+
 	}
 
 }
